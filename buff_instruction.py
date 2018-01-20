a78e547a21d55679a25909a808e93d1f242baf93
Maxym Hybalo <esdth.mo@gmail.com>
2018-01-20 12:26:10 +0200

    refactoring buff instruction

--- buff_instruction.py
+++ buff_instruction.py
@@ -3,6 +3,18 @@ import json
 
 CONFIG = 'buff_utils.json'
 
+buff_squence = [
+	[4, [0], [2]],
+	[5, [0], [2]],
+	[6, [0], [2]],
+	[3, [0], [2]],
+	[0, [0,1,2,3,4], [2 ] * 5],
+	[1, [0,1,2,3,4], [2] * 5],
+	[2, [0], [2]],
+	[7, [1,2,0], [2,2,5]]
+]
+
+
 def buffs(clicks, delays):
 	result = list()
 	multiply = 37
@@ -34,56 +46,11 @@ def getBufferCommand(id):
 	}
 
 
-char_selector_cond = {
-			process : 'recognize',
-			region : (740, 130, 20, 20),
-			img : IMG_PREFIX + 'load_marker.png',
-			freq : 3
-		}
-
-
 def await_(_delay):
 	return {
 		process: 'wait',
 		delay: _delay
 	}
-
-go_to_select_menu_v2 = [
-	{
-		process: 'center_on',
-		region: (940, 740, 120, 120),
-		img: IMG_PREFIX + 'menu_btn.png',
-		freq: 1
-	},
-	{
-		process: 'center_on',
-		region: (955, 710,170, 20),
-		img: IMG_PREFIX + 'system_menu.png',
-		freq: 1
-	},
-	{
-		process: 'center_on',
-		region: (500, 400, 200, 30),
-		img: IMG_PREFIX + 'to_char_select.png',
-		freq: 1
-	},
-		{
-		process: 'center_on',
-		region: (580, 230, 100, 30),
-		img: IMG_PREFIX + 'ok_btn.png',
-		freq: 1
-	},
-]
-
-
-
-
-load_char = {
-	process : 'click',
-	x : 930,
-	y : 570,
-	delay : 1
-}
 
 
 def make_single_buff(id, positions, delays, cfg=None):
@@ -105,16 +72,6 @@ def make_reload_instruction(id, cfg=None):
 		await_(13)
 	] + cfg['select_char_menu']
 
-buff_squence = [
-	[4, [0], [2]],
-	[5, [0], [2]],
-	[6, [0], [2]],
-	[3, [0], [2]],
-	[0, [0,1,2,3,4], [2 ] * 5],
-	[1, [0,1,2,3,4], [2] * 5],
-	[2, [0], [2]],
-	[7, [1,2,0], [2,2,5]]
-]
 
 def instruction(sequence):
 	cfg = import_config()
@@ -129,33 +86,6 @@ def instruction(sequence):
 def getBuffInstruction():
 	return instruction(buff_squence)
 
-
-to_select_char_menu = [
-		{
-			process: 'click',
-			x: 1005,
-			y: 773,
-			delay: 1
-		},
-		{
-			process: 'click',
-			x: 1005,
-			y: 723,
-			delay: 1
-		},
-		{
-			process: 'click',
-			x: 600,
-			y: 413,
-			delay: 1
-		},
-		{
-			process: 'click',
-			x: 635,
-			y: 234,
-			delay: 1
-		}
-	]
 
 def import_config():
 	config = open(CONFIG, 'r')
