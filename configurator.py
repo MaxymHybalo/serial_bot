918bbd448eb5eaff20b8573e64a29b2acd7eb52f
Maxym Hybalo <keepass.log@gmail.com>
2018-03-10 05:03:05 -0800

    configurator refactoring

--- configurator.py
+++ configurator.py
@@ -2,18 +2,17 @@ import json
 from recognizer import Recognizer
 from click import Click
 
-class Configurator: 
-    
+
+class Configurator:
+
     def __init__(self, filepath):
         self.filepath = filepath
-    
 
     def import_config(self):
         config = open(self.filepath, 'r')
         configuration = config.read()
         config.close()
         return json.loads(configuration)
-
 
     def generate_objects(self):
         config = self.import_config()
@@ -30,7 +29,7 @@ class Configurator:
         for e in value:
             sub_list.append(self._get_object(e))
         return sub_list
-    
+
     def _get_object(self, cfg):
         process = cfg['process']
         if process == 'recognize':
