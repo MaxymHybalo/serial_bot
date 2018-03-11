7cca92ba3d22b579c4ba32ac1a41e505f6e04f59
Maxym Hybalo <keepass.log@gmail.com>
2018-03-11 11:04:45 -0700

    add test env for cv, mockup color detection

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import time
+import pyautogui as ui
 import buff_instruction as buff
 import enhance
 
 from processors import InstructionProcessor
 from click import Click
 from configurator import Configurator
+from recognizer import Recognizer
 
 if __name__ == '__main__':
     startTime = time.time()
@@ -13,8 +15,11 @@ if __name__ == '__main__':
     # processInstruction(enhance.enhance())
     # processInstruction(buff.getBuffInstruction())
     # processor = InstructionProcessor(buff.getBuffInstruction())
-    processor = InstructionProcessor(enhance.enhance(enhance.combination))
-
+    # processor = InstructionProcessor(Click(500, 300))
+    time.sleep(2)
+    img = ui.screenshot(region=(242, 32, 337, 480))
+    rec = Recognizer(img, None, **{'find_color': [[0], [0]]}, process='find')
+    processor = InstructionProcessor([rec,])
     processor.process()
     execTime = (time.time() - startTime)
 
--- recognizer.py
+++ recognizer.py
@@ -1,19 +1,23 @@
 import pyautogui as ui
 import time
+import numpy as np
+import cv2
+
 
 class Recognizer:
 
-    def __init__(self, image, region, wait=1, process="recognize"):
+    def __init__(self, image, region, wait=1, process="recognize", **kwargs):
         self.image = image
         self.region = region
         self.wait = wait
         self.process = process
+        print(kwargs)
 
     def recognize(self):
         self.value = ui.locateOnScreen(str(self.image), region= self.region)
         while self.value is None:
             time.sleep(self.wait)
-            self.value = ui.locateOnScreen(str(self.image), region= self.region)
+            self.value = ui.locateOnScreen(str(self.image), region=self.region)
         return self.value
 
     def center_of(self):
@@ -21,6 +25,27 @@ class Recognizer:
             'x': self.value[0] + self.value[2] / 2,
             'y': self.value[1] + self.value[3] / 2
         }
+
+    def find(self):
+        # convert to cv2 format
+        self.image = np.array(self.image)
+        self.image = self.image[:, :, ::-1]
+        self.image = self._extract_color(self.image, [[0, 50, 50], [5, 255, 255]])
+        cv2.imshow('image', self.image)
+
+        # show options
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    # color is array from first vector is lower color component, second is upper color component
+    @staticmethod
+    def _extract_color(image, color):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower = np.array(color[0])
+        upper = np.array(color[1])
+        mask = cv2.inRange(image, lower, upper)
+        image = cv2.bitwise_and(image, image, mask=mask)
+        return cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
 
     def __str__(self):
         return 'image: ' + self.image + ' region: ' + str(self.region)
