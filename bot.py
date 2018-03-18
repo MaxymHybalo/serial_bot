782af8d43ee1504f39e77c949a582c26dffd31dc
Maxym Hybalo <keepass.log@gmail.com>
2018-03-18 04:19:34 -0700

    clean buff file

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
 import time
-import pyautogui as ui
 import buff_instruction as buff
 import enhance
 
 from processors import InstructionProcessor
-from click import Click
-from configurator import Configurator
-from recognizer import Recognizer
 
 if __name__ == '__main__':
     startTime = time.time()
-    # run()
-    # print(buff.getBuffInstruction())
-    # processInstruction(enhance.enhance())
-    # processInstruction(buff.getBuffInstruction())
     processor = InstructionProcessor(buff.get_buff_instruction(sequence=buff.farm_buff_sequence, reload=True))
-    # processor = InstructionProcessor(Click(500, 300))
-    # img = ui.screenshot(region=(242, 32, 337, 480))
-    kwargs = {
-        'roi': (0, 0, 280, 480),
-        'color': ((0, 50, 50), (1, 255, 255)),
-        'kernel': (2, 2)
-    }
+    # following object used as example for recognizer
+    # kwargs = {
+    #     'roi': (0, 0, 280, 480),
+    #     'color': ((0, 50, 50), (1, 255, 255)),
+    #     'kernel': (2, 2)
+    # }
     # rec = Recognizer('assets/inventory.JPG', None, **kwargs, process='find')
     # processor = InstructionProcessor([rec,])
     processor.process()
