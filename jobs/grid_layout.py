58bd8e7e17e7a6937f84fa286949995614f2f241
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2018-04-05 16:45:54 +0300

    logger for image

--- jobs/grid_layout.py
+++ jobs/grid_layout.py
@@ -1,14 +1,12 @@
 from processes.recognizer import Recognizer
-from utils import cv2_utils as utils
+import utils.cv2_utils as utils
 
 class Grid:
 
     def __init__(self, identifier):
         self.identifier = identifier
         start = self.__find_grid_entry()
-        image = utils.make_image()
-        image = utils.draw_rect(image, start)
-        utils.show(image)
+        utils.log_image({'rect': start})
 
     def __find_grid_entry(self):
         return Recognizer(self.identifier, None).recognize()
--- utils/cv2_utils.py
+++ utils/cv2_utils.py
@@ -2,15 +2,15 @@ import cv2
 import pyautogui as ui
 import numpy as np
 
+COLOR = (0, 255, 0)
+THINKNESS = 2
 
 def get_image(imagepath):
     if type(imagepath) is str:
         return cv2.imread(imagepath)
     if type(imagepath) in [list, tuple]:
         image = ui.screenshot(region=imagepath)
-        # convert to cv2 format
         image = np.asarray(image)
-        # image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
         return image
     return imagepath
 
@@ -28,9 +28,12 @@ def draw_corners(image, corners, color=255):
 
 def draw_rect(image, rect):
     x, y, w, h = rect
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 179), 2)
+    cv2.rectangle(image, (x, y), (x + w, y + h), COLOR, THINKNESS)
     return image
 
+def draw_circle(image, circle):
+    x, y, r = circle
+    cv2.circle(image, (x,y), r, COLOR, THINKNESS)
 
 def make_image(region=None):
     if region is None:
@@ -42,3 +45,13 @@ def show(image, name='image'):
     cv2.imshow(name, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def log_image(**kwargs):
+    image = make_image()
+    for key, value in kwargs:
+        if key is 'rect':
+            image = draw_rect(image, value)
+        if key is 'circle':
+            image = draw_circle
+    show(image)
