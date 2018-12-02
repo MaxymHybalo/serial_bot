132638f21cf1726ac31ede2a909a70dce602c497
Maxym Hybalo <keepass.log@gmail.com>
2018-12-02 08:31:00 -0800

    init imline base ui

--- launcher.py
+++ launcher.py
@@ -1,11 +1,14 @@
 import telebot
 from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 import handlers
 import logging
 from utils.configurator import Configurator
 
 CONFIG_FILE = 'config.yml'
 
+MODES = ['buff', 'spawn', 'logout', 'enhance']
 def configure_logger():
     log_format = '%(levelname)s : %(name)s %(asctime)s - %(message)s'
     logging.basicConfig(level=logging.DEBUG,
@@ -21,12 +24,7 @@ bot = telebot.TeleBot(config['token'])
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
-    item_buff = types.KeyboardButton('buff')
-    item_spawn = types.KeyboardButton('spawn')
-    item_logout = types.KeyboardButton('logout')
-    item_enhance = types.KeyboardButton('enhance')
-    markup.row(item_buff, item_spawn, item_logout, item_enhance)
+    markup = create_base_keyboard()
     bot.send_message(message.chat.id, "Let's start work", reply_markup=markup)
 
 @bot.message_handler(commands=['config'])
@@ -60,44 +58,49 @@ def cycles_handler(message):
     mode = validate(message, message.chat.id)
     if len(mode) > 1:
         handlers.set_cycles(mode, config)
-    bot.send_message(message.chat.id, 'Maybe I update cycles count')
 
 def spawn(message):
     handlers.set_spawn(config)
     handlers.run_bot()
-    bot.send_message(message.chat.id, 'Okay! Returned')
 
 def logout(message):
     handlers.set_logout(config)
     handlers.run_bot()
-    bot.send_message(message.chat.id, 'Okay! Spawn')
 
 def buff(message):
-    bot.send_message(message.chat.id, 'Okay! I start buffing, please be patient')
     params = message.text.split(' ')
     handlers.set_buff(params, config)
     handlers.set_mode('buff', CONFIG_FILE)
-    final = handlers.run_bot()
-    bot.send_message(message.chat.id, 'Great! You can go. Buff ended at ' + str(final/60))
 
 def enhance(message):
     handlers.set_mode('enhance', CONFIG_FILE)
-    time = handlers.run_bot()
-    bot.send_message(message.chat.id, 'Good, that\'s all, just in ' + str(time/60))
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    modes = ['spawn', 'buff', 'enhance', 'logout']
     command = message.text
-    for m in modes:
+    for m in MODES:
         if command == m:
             globals()[m](message)
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_base_callbacks(call):
+    bot.answer_callback_query(call.id, 'Start ' + call.data)
+    globals()[call.data]()
+    bot.answer_callback_query(call.id, 'End ' + call.data)
+            
 
 def validate(params, id):
     mode = params.text.split(' ')
     if len(mode) <= 1:
         bot.send_message(id, 'You forget give me mode name')
     return mode
+
+def create_base_keyboard():
+    markup = InlineKeyboardMarkup()
+    keyboard = MODES
+    for key in keyboard:
+        markup.add(InlineKeyboardButton(key, callback_data=key))
+    return markup
 
 if not config['debug']:
     bot.polling()
