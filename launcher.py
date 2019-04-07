47ba1dfd5c0889c18a431393fb7e83247103009a
Maxym Hybalo <keepass.log@gmail.com>
2019-04-07 16:15:29 +0300

    reply inline keyboard markup

--- launcher.py
+++ launcher.py
@@ -38,27 +38,24 @@ def combination(message):
 def cycles(cycle):
     handlers.set_mode('enhance', CONFIG_FILE)
     handlers.set_cycles(cycle, config)
-    print(chat_id, message_id)
-    # bot.edit_message_reply_markup(
-    #     chat_id=chat_id,
-    #     message_id=message_id,
-    #     inline_message_id='Cycles ' + str(cycle)
-    # )
     handlers.run_bot()
     return None
 
 def spawn():
     handlers.set_spawn(config)
     handlers.run_bot()
+    return create_base_keyboard()
 
 def logout():
     handlers.set_logout(config)
     handlers.run_bot()
+    return create_base_keyboard()
 
 def buff():
     handlers.set_mode('buff', CONFIG_FILE)
     handlers.set_buff(['buff'], config)
     handlers.run_bot()
+    return create_base_keyboard()
 
 def handle_child_nodes(data):
     if data[0] == 'cycle':
@@ -104,7 +101,6 @@ def create_base_keyboard():
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_base_callbacks(call):
-    print("##########", call.id, call.data)
     bot.answer_callback_query(call.id, 'Start ' + call.data)
     data = call.data.split('_')
     if data[0] == 'child':
