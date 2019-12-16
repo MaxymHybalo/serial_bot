0b3c1a1533c3cef231fb479d2e9a6bff3ada7efc
Maxym Hybalo <keepass.log@gmail.com>
2019-12-16 21:08:49 +0200

    upgrade farming tools

--- jobs/farming.py
+++ jobs/farming.py
@@ -6,22 +6,73 @@ from processes.move import Move
 from shapes.window import Window
 
 class Farming:
+    
+
+    actions = {
+        'one': 2.4,
+        'two': 0.4,
+        'three': 0.4,
+        'four': 80,
+        'turn': 6
+    }
+
+    actionsState = {
+        'one': 0,
+        'two': 0,
+        'three': 0,
+        'four': 0,
+        'turn': 0
+    }
 
     def __init__(self):
         self.config = Config()
         self.config.enable()
         self.roundFrequency = 10
+        self.actionsState = self.init_actions_time(self.actionsState)
+
         self.window = Window()
         self.roundStart = self.window.center()
-        self.rountEnd = self.roundStart[0] + 100, self.roundStart[1]
+        self.rountEnd =  self.roundStart[0] + 100, self.roundStart[1]
 
     def run(self):
         startTime = time.time()
 
         while self.config.isWorks():
-            Key('1').press()
-            time.sleep(3)
-            currentTime = time.time()
-            if startTime + self.roundFrequency < currentTime:
-                startTime = currentTime
-                Move().fromTo(self.roundStart, self.rountEnd)
+            print('[While iteration]')
+            for a in self.actionsState:
+                itime = time.time()
+                if itime > self.actionsState[a]:
+                    f = getattr(self, a)
+                    f()
+                    self.actionsState[a] = itime + self.actions[a]
+    
+    def init_actions_time(self, actions):
+        itime = time.time()
+        for a in actions:
+            actions[a] = itime
+        return actions
+
+    def one(self):
+        print('1')
+        Key('1').press()
+        return None
+
+    def two(self):
+        print('2')
+        Key('2').press()
+        return None
+
+    def three(self):
+        print('3')
+        Key('3').press()
+        return None
+
+    def four(self):
+        print('4')
+        Key('4').press()
+        return None
+
+    def turn(self):
+        print('turn')
+        Move().fromTo(self.roundStart, self.rountEnd)
+        return None
