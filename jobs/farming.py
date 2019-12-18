93ab0bc62ee6b0537cb0fc1a5b9c5c6b00112b44
Maxym Hybalo <keepass.log@gmail.com>
2019-12-18 21:59:59 +0200

    enhance farming tools

--- jobs/farming.py
+++ jobs/farming.py
@@ -2,75 +2,78 @@ import time
 from utils.config import Config
 from processes.key import Key
 from processes.move import Move
+from processes.wait import Wait
 from shapes.window import Window
 
 class Farming:
     # TODO invistigate asyncio
+    frequences = [
+        2,
+        0.3,
+        0.3,
+        15,
+        15,
+        40,
+        40,
+        40,
+        6
+    ]
 
-    actions = {
-        'one': 2.4,
-        'two': 0.4,
-        'three': 0.4,
-        'four': 80,
-        'turn': 6
-    }
-
-    actionsState = {
-        'one': 0,
-        'two': 0,
-        'three': 0,
-        'four': 0,
-        'turn': 0
-    }
+    action_params = [
+        '1',
+        '2',
+        '3',
+        '5',
+        '6',
+        ['a', '5'], # alt + 1
+        ['a', '4'],
+        ['a', '1']3,
+        '_'
+    ]
 
     def __init__(self):
         self.config = Config()
         self.config.enable()
         self.roundFrequency = 10
-        self.actionsState = self.init_actions_time(self.actionsState)
-
-        # self.window = Window()
-        # self.roundStart = self.window.center()
-        # self.rountEnd =  self.roundStart[0] + 100, self.roundStart[1]
+        
+        self.generate_actions()
+        self.window = Window()
+        self.roundStart = self.window.center()
+        self.rountEnd =  self.roundStart[0] + 100, self.roundStart[1]
 
     def run(self):
         startTime = time.time()
         while self.config.isWorks():
-            print('[While iteration]')
-            for a in self.actionsState:
+            # print('[While iteration]')
+            for i, a in enumerate(self.actions):
                 itime = time.time()
-                if itime > self.actionsState[a]:
+                if itime > self.skills_state[i]:
                     f = getattr(self, a)
-                    f()
-                    self.actionsState[a] = itime + self.actions[a]
+                    f(self.action_params[i])
+                    print(a, self.frequences[i])
+                    self.skills_state[i] = itime + self.frequences[i]
     
-    def init_actions_time(self, actions):
+    def init_actions_time(self):
         itime = time.time()
-        for a in actions:
-            actions[a] = itime
-        return actions
+        for a in range(len(self.actions)):
+            self.skills_state[a] = itime
+    
+    def generate_actions(self):
+        self.actions = ['key'] * 8
+        self.actions.append('turn')
+        self.skills_state = [0] * 9
+        self.init_actions_time()
 
-    def one(self):
-        print('1')
-        Key('1').press()
-        return None
+    def key(self, params):
+        if type(params) == list:
+            print('list key call')
+            Key(params[1], pressed=params[0]).combination()
 
-    def two(self):
-        print('2')
-        Key('2').press()
-        return None
+        else:
+            Key(params).press()
+        Wait(0.4).delay()
 
-    def three(self):
-        print('3')
-        Key('3').press()
-        return None
-
-    def four(self):
-        print('4')
-        Key('4').press()
-        return None
-
-    def turn(self):
+    def turn(self, _):
         print('turn')
-        # Move().fromTo(self.roundStart, self.rountEnd)
+        Move().fromTo(self.roundStart, self.rountEnd)
         return None
