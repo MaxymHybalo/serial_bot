6e0435cb16b88fea5afe8b63310a1ec5bc9e8024
Maxym Hybalo <keepass.log@gmail.com>
2020-09-05 19:57:24 +0300

    generate cells

--- enhancer/grid_identifier.py
+++ enhancer/grid_identifier.py
@@ -39,75 +39,83 @@ class GridIdentifier:
         # log cells
         # for c in self.matix_rects:
         #     self.source = utils.rect(self.source, c.rect())
+        #     self.source = utils.text(self.source, c.number, x=c.x, y=c.y)
+
         # utils.show_image(self.source)
+        
 
-    def slice_inventory(self, start, end):
-        self.log.debug('Try slice scope {0}:{1}'.format(start, end))
-        slice = list()
-        start_line = self.matix_rects[start[1] - 1][start[0] - 1:self.col]
-        slice.append(start_line)
-        center = []
-        if end:
-            end_line = self.matix_rects[end[1]][:end[0]]
-            center = self.matix_rects[start[1]:end[1]]
-            slice.append(end_line)
-        else:
-            center = self.matix_rects[start[1]:11]
-        for c in center:
-            slice.append(c)
-        self.write_2nd_rects(slice, 'testslice.png')
-        return slice
+    # def slice_inventory(self, start, end):
+    #     self.log.debug('Try slice scope {0}:{1}'.format(start, end))
+    #     slice = list()
+    #     start_line = self.matix_rects[start[1] - 1][start[0] - 1:self.col]
+    #     slice.append(start_line)
+    #     center = []
+    #     if end:
+    #         end_line = self.matix_rects[end[1]][:end[0]]
+    #         center = self.matix_rects[start[1]:end[1]]
+    #         slice.append(end_line)
+    #     else:
+    #         center = self.matix_rects[start[1]:11]
+    #     for c in center:
+    #         slice.append(c)
+    #     self.write_2nd_rects(slice, 'testslice.png')
+    #     return slice
 
-    def get_center_of(self, col, row):
-        """
-        :param col: column of item
-        :param row: row of item
-        :return: center coordinates of item cell
-        """
-        item = self.matix_rects[row - 1][col - 1]
-        return int(item[0] + item[2] / 2), int(item[1] + item[3] / 2)
+    # def get_center_of(self, col, row):
+    #     """
+    #     :param col: column of item
+    #     :param row: row of item
+    #     :return: center coordinates of item cell
+    #     """
+    #     item = self.matix_rects[row - 1][col - 1]
+    #     return int(item[0] + item[2] / 2), int(item[1] + item[3] / 2)
 
     def get_region_of(self, col, row):
-        """
-        :param col: column of item position
-        :param row: row of item position
-        :return: rectangle of item area
-        """
-        return self.matix_rects[row - 1][col - 1]
+        # """
+        # :param col: column of item position
+        # :param row: row of item position
+        # :return: rectangle of item area
+        # """
+        # return self.matix_rects[row - 1][col - 1]
 
-    def find_position(self, target):
-        """
-        :param target: path to item image
-        :return: col and row of found
-        """
-        self.log.debug('Find target at inventory')
-        insertion = Recognizer(target, self.inventory_region, wait=0).recognize(once=True)
-        if insertion is None:
-            return None
-        rect = list(insertion)
-        rect[0], rect[1], rect[2], rect[3] = rect[0] - 1, rect[1] - 2, ITEM_WIDTH + 1, ITEM_HEIGHT + 1
-        for row in self.matix_rects:
-            if rect in row:
-                x, y = row.index(list(rect)), self.matix_rects.index(row)
-        self.log.debug('Found target at position: {0}'.format([x, y]))
-        if self.debug:
-            self.write_2nd_rects(self.matix_rects[y][x], 'log/eoi.png', single_rect=True)
-        return x, y
+    # def find_position(self, target):
+    #     """
+    #     :param target: path to item image
+    #     :return: col and row of found
+    #     """
+    #     self.log.debug('Find target at inventory')
+    #     insertion = Recognizer(target, self.inventory_region, wait=0).recognize(once=True)
+    #     if insertion is None:
+    #         return None
+    #     rect = list(insertion)
+    #     rect[0], rect[1], rect[2], rect[3] = rect[0] - 1, rect[1] - 2, ITEM_WIDTH + 1, ITEM_HEIGHT + 1
+    #     for row in self.matix_rects:
+    #         if rect in row:
+    #             x, y = row.index(list(rect)), self.matix_rects.index(row)
+    #     self.log.debug('Found target at position: {0}'.format([x, y]))
+    #     if self.debug:
+    #         self.write_2nd_rects(self.matix_rects[y][x], 'log/eoi.png', single_rect=True)
+    #     return x, y
 
     def generate_rectangles(self, start):
         self.log.debug('Try generate rectangles: {0}, {1}'.format(self.col, self.row))
         x, y = start
         cells = []
+        count = 0
         for col in range(self.col):
             for row in range(self.row):
                 dx = x + (ITEM_WIDTH + MARGIN) * col + col
-                dy = y + (ITEM_HEIGHT + MARGIN + 1) * row + row
+                dy = y + 1 + (ITEM_HEIGHT + MARGIN + 1) * row + row
+                source = self.source[dy:dy+ITEM_HEIGHT, dx:dx+ITEM_WIDTH]
                 config = {
                     "col": col,
                     "row": row,
                     "x": dx,
-                    "y": dy
+                    "y": dy,
+                    "source": source,
+                    "number": count
                 }
+                count+=1
                 cells.append(Cell(**config))
         # import pdb; pdb.set_trace()
         self.log.debug('Cells generated')
@@ -124,5 +132,5 @@ class GridIdentifier:
     def _inventory_region(self):
         self.log.debug('Find inventory region')
         region = [self.entry[0] - 1, self.entry[1] - 1, self.col * (ITEM_WIDTH + 1) + self.col + 1,
-                  self.row * (ITEM_HEIGHT + 2) + self.row + 1]
+                  self.row * (ITEM_HEIGHT + 1) + self.row + 1]
         return region
