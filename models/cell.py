37d2554ea21bf6c225d43298c106df2100091431
Maxym Hybalo <keepass.log@gmail.com>
2020-10-19 23:44:43 +0300

    add binary data to cells table

--- models/cell.py
+++ models/cell.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey
+from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey, LargeBinary
 from sqlalchemy.orm import mapper
 
 metadata = MetaData()
@@ -9,16 +9,19 @@ cell = Table('cells', metadata,
     Column('row', Integer),
     Column('x', Integer),
     Column('y', Integer),
-    Column('grid_id', Integer))
+    Column('grid_id', Integer),
+    Column('source', LargeBinary))
 
 class Cell(object):
-    def __init__(self, col, row, x, y, gid):
+    def __init__(self, col, row, x, y, source, gid):
         self.col     = col
         self.row     = row
         self.x       = x
         self.y       = y
+        self.source  = source
         self.grid_id = gid
 
     def __repr__(self):
-        return 'Cell({0}, {1}, {2}, {3}, {4}'.format(self.col, self.row, self.x, self.y, self.grid_id)
+        return 'Cell({0}, {1}, {2}, {3}, {4})'.format(self.col, self.row, self.x, self.y, self.grid_id)
+
 mapper(Cell, cell)
--- test_db.py
+++ test_db.py
@@ -2,21 +2,32 @@ from sqlalchemy import create_engine, text, MetaData
 from models.cell import Cell, metadata
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+import cv2 
+import numpy as np
+import pickle
+import utils.cv2_utils as u
 connect_url = 'postgresql+psycopg2://dev:123@localhost:5432/serial_bot_dev'
 
 db = create_engine(connect_url)
-# import pdb; pdb.set_trace()
+# metadata.bind = db
+# metadata.create_all()
+image = cv2.imread('db_blob.png')
+image = np.array(image)
+as_bytes = image.dumps()
 
 Session = sessionmaker(bind=db)
 session = Session()
-cell = Cell(2,2,3,4,5)
+cell = Cell(2,2,3,4, as_bytes, 1)
 session.add(cell)
 session.commit()
 
-cells = session.query(Cell).all()
-# with db.engine.connect() as c:
-#     query = text('SELECT * FROM test')
-#     tables = c.execute(query)
-#     import pdb; pdb.set_trace()
-#     print(tables[0])
-# print(db)
+table_row = session.query(Cell).first()
+print(table_row.source)
+source = table_row.source
+
+image = pickle.loads(source)
+
+# print(size(as_bytes))
+# print(pickle.loads(as_bytes))
+u.show_image(image)
